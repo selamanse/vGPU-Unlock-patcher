3714c6c918fe9803dd09a52526672d4a1b501a74
GreenDamTan <GreenDamTan@users.noreply.github.com>
2024-05-02 19:18:04 +0800

    add mining card in patch.sh vcfgclone

--- patch.sh
+++ patch.sh
@@ -641,6 +641,9 @@ if $DO_VGPU; then
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1E30 0x12BA 0x1E81 0x0000	# RTX 2080 super 8GB
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1E30 0x12BA 0x1f03 0x0000	# RTX 2060 12GB
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1E30 0x12BA 0x1f11 0x0000	# RTX 2060 Mobile 6GB
+    vcfgclone ${TARGET}/vgpuConfig.xml 0x1E30 0x12BA 0x1E09 0x0000	# CMP 50HX -> Quadro RTX 6000
+    vcfgclone ${TARGET}/vgpuConfig.xml 0x1E30 0x12BA 0x1F0B 0x0000	# CMP 40HX -> Quadro RTX 6000
+    vcfgclone ${TARGET}/vgpuConfig.xml 0x1E30 0x12BA 0x2189 0x0000	# CMP 30HX -> Quadro RTX 6000
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1E30 0x12BA 0x2184 0x0000	# GTX 1660 6GB
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1E30 0x12BA 0x1f95 0x0000	# GTX 1650 Ti Mobile 4GB
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1E30 0x12BA 0x1EB1 0x0000	# Quadro RTX 4000
@@ -649,6 +652,10 @@ if $DO_VGPU; then
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1B38 0x0 0x1B00 0x0000		# TITAN X
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1B38 0x0 0x1B06 0x0000		# GTX 1080 Ti
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1B38 0x0 0x1B81 0x0000		# GTX 1070
+    vcfgclone ${TARGET}/vgpuConfig.xml 0x2233 0x165A 0x1B07 0x0000	# P102-100-> A5500
+    vcfgclone ${TARGET}/vgpuConfig.xml 0x2233 0x165A 0x1B87 0x0000	# P104-100-> A5500
+    vcfgclone ${TARGET}/vgpuConfig.xml 0x2233 0x165A 0x1C07 0x0000	# P106-100-> A5500
+    vcfgclone ${TARGET}/vgpuConfig.xml 0x2233 0x165A 0x1C09 0x0000	# P106-090-> A5500
     vcfgclone ${TARGET}/vgpuConfig.xml 0x1B38 0x0 0x1D01 0x0000		# GTX 1030 -> Tesla P40
     vcfgclone ${TARGET}/vgpuConfig.xml 0x13F2 0x0 0x17FD 0x0000		# Tesla M40 -> Tesla M60
     vcfgclone ${TARGET}/vgpuConfig.xml 0x13F2 0x0 0x13C0 0x0000		# GTX 980 -> Tesla M60
