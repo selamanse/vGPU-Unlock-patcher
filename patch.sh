10848dc9a509843cf99aa1fc4a755265de4690ed
snowman <snowman@localhost.localdomain>
2024-06-09 21:02:44 +0200

    compat patch for stricter gcc - drm_gem_object_vmap
    cherry picked from https://github.com/NVIDIA/open-gpu-kernel-modules
    550.67 tag
    
    thanks to Pesc0 user of vgpu unlock discord for pointing out to
    the original fix posted in nvidia forums

--- patch.sh
+++ patch.sh
@@ -660,6 +660,8 @@ if $DO_VGPU; then
     echo
 fi
 
+applypatch ${TARGET} kernel-driver-fix-drm_gem_object_vmap.patch
+
 $ENVYPROBES && {
     applypatchx ${TARGET} envy_probes-ioctl-hooks-from-mbuchel.patch
     sed -e '/^NVIDIA_CFLAGS += .*BIT_MACROS$/aNVIDIA_CFLAGS += -DENVY_LINUX' -i ${TARGET}/kernel{,-open}/nvidia/nvidia.Kbuild
